49b50de9d058a44b6744e3ea15a2b6c493a82a2c
leonyurko <59198752+leonyurko@users.noreply.github.com>
2025-04-14 23:22:35 +0300

    Added initial Flutter screens and fixed navigation

---
+++ focusease/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:focusease/screens/login_screen.dart';
+import 'package:focusease/screens/register_screen.dart';
+import 'package:focusease/screens/tasks_screen.dart';
+import 'package:focusease/screens/media_screen.dart';
+
+void main() {
+  runApp(const FocusEaseApp());
+}
+
+class FocusEaseApp extends StatelessWidget {
+  const FocusEaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FocusEase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/tasks': (context) => const TasksScreen(),
+        '/media': (context) => const MediaScreen(),
+      },
+    );
+  }
+}
